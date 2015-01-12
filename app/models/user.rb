4d874599fbc375738098acec2e11e87d546c3c8b
Justin Lloyd <justin.m.lloyd@gmail.com>
2015-01-12 09:54:28 -0700

    Finish log in/out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 
+	attr_accessor :remember_token
+
 	before_save { self.email = email.downcase }
 
 	validates :name, 
@@ -18,5 +20,33 @@ class User < ActiveRecord::Base
 
 	validates :password,
 		length: { minimum: 6 }
+	
+	#Remember a user in the database for use in persistent sessions
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
 
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
+	#Returns the hash digest of the given string.
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
+
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+
+	#Returns true if the given token matches the digest.
+	def authenticated?(remember_token)
+		if remember_digest.nil?
+			false
+		else
+			BCrypt::Password.new(remember_digest).is_password?(remember_token)
+		end
+	end
 end

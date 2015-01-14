75ea21620651ea61268cb3f9daba7a09dcf354bf
Justin Lloyd <justin.m.lloyd@gmail.com>
2015-01-14 11:23:14 -0700

    Add account activations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
-	attr_accessor :remember_token
+	attr_accessor 	:remember_token, :activation_token
 
-	before_save { self.email = email.downcase }
+	before_save 	:downcase_email
+	before_create 	:create_activation_digest
 
 	validates :name, 
 		presence: true, 
@@ -43,11 +44,31 @@ class User < ActiveRecord::Base
 	end
 
 	#Returns true if the given token matches the digest.
-	def authenticated?(remember_token)
-		if remember_digest.nil?
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		if digest.nil?
 			false
 		else
-			BCrypt::Password.new(remember_digest).is_password?(remember_token)
+			BCrypt::Password.new(digest).is_password?(token)
 		end
 	end
+
+	def activate
+		update_attribute(:activated,		true)
+		update_attribute(:activated_at,		Time.zone.now)
+	end
+
+	def send_activation_email
+		UserMailer.account_activation(self).deliver_now
+	end
+
+	private
+		def downcase_email
+			self.email = email.downcase 
+		end
+
+		def create_activation_digest
+			self.activation_token	= User.new_token
+			self.activation_digest 	= User.digest(activation_token)
+		end
 end
